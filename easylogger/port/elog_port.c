5d24d89de36022f883f04abeea7593527da07b1a
chxaitz <345473553@qq.com>
2015-06-04 22:02:38 +0800

    Update elog_port.c
    1.增加了设备流的打开、输入、关闭三个函数，这三个函数，会再需要向流写入数据时调用。

--- easylogger/port/elog_port.c
+++ easylogger/port/elog_port.c
@@ -16,11 +16,14 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Function: Portable interface for each platform.
+ * Function: Portable interface for RT-Thread.
  * Created on: 2015-04-28
  */
- 
+
 #include "elog.h"
+#include "time.h"
+#include "usart.h"
+#include "rtc.h"
 
 /**
  * EasyLogger port initialize
@@ -30,8 +33,6 @@
 ElogErrCode elog_port_init(void) {
     ElogErrCode result = ELOG_NO_ERR;
 
-	//add your code here
-	
     return result;
 }
 
@@ -39,38 +40,36 @@ ElogErrCode elog_port_init(void) {
  * output log port interface
  */
 void elog_port_output(const char *output, size_t size) {
-	
-    //add your code here
-	
+    /* output to terminal */
+    usart_write( 0, output, size);
+    //TODO output to flash
 }
 
 /**
  * output lock
  */
 void elog_port_output_lock(void) {
-	
-    //add your code here
-	
 }
 
 /**
  * output unlock
  */
 void elog_port_output_unlock(void) {
-	
-    //add your code here
-	
 }
-
+/* be modified   at 2015-06-04 17:09 by chxaitz */
 /**
  * get current time interface
  *
  * @return current time
  */
 const char *elog_port_get_time(void) {
-	
-    //add your code here
-	
+  time_t tm_cur;
+  struct tm *px_tm;
+  static char cur_system_time[21] = { 0 };
+  tm_cur = rtc_get_time_t();
+  px_tm = localtime( &tm_cur );
+  sprintf(cur_system_time,"%4d-%02d-%02d %02d:%02d:%02d", (1900+px_tm->tm_year), (1+px_tm->tm_mon), px_tm->tm_mday, px_tm->tm_hour, px_tm->tm_min, px_tm->tm_sec);
+  return cur_system_time;
 }
 
 /**
@@ -79,18 +78,40 @@ const char *elog_port_get_time(void) {
  * @return current process name
  */
 const char *elog_port_get_p_info(void) {
-	
-    //add your code here
-	
+    return "";
 }
 
 /**
  * get current thread name interface
- *
- * @return current thread name
  */
 const char *elog_port_get_t_info(void) {
-	
-    //add your code here
-	
+    return "";
+}
+/* be added   at 2015-06-04 17:09 by chxaitz */
+/**
+ * open then output stream device
+ */
+ElogErrCode elog_port_open()
+{
+  ElogErrCode res = ELOG_NO_ERR;
+  return res;
+}
+/* be added   at 2015-06-04 17:09 by chxaitz */
+/**
+ * write data to the stream device
+ */
+ElogErrCode elog_port_write( char *buf, size_t len )
+{
+  ElogErrCode res = ELOG_NO_ERR;
+  usart_write( 0, buf, len );
+  return res;
+}
+/* be added   at 2015-06-04 17:09 by chxaitz */
+/**
+ * close then output stream device
+ */
+ElogErrCode elog_port_close()
+{
+  ElogErrCode res = ELOG_NO_ERR;
+  return res;
 }
